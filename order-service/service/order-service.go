d15590779b07103ac04049c8de114b756117701f
Sayp <officialsayp@proton.me>
2026-09-19 00:45:01 +0300

    refactor: inject product availability into order service

--- order-service/service/order-service.go
+++ order-service/service/order-service.go
@@ -4,11 +4,27 @@ import "errors"
 
 var ErrProductUnavailable = errors.New("product is unavailable")
 
-type OrderService struct{}
+type OrderService struct {
+	productAvailability ProductAvailability
+}
 
 func (o *OrderService) CreateOrder(product string) error {
-	if product == "unavailable" {
+	available, err := o.productAvailability.IsAvailable(product)
+	if err != nil {
+		return err
+	}
+	if !available {
 		return ErrProductUnavailable
 	}
 	return nil
+}
+
+type ProductAvailability interface {
+	IsAvailable(product string) (bool, error)
+}
+
+func NewOrderService(productAvailability ProductAvailability) *OrderService {
+	return &OrderService{
+		productAvailability: productAvailability,
+	}
 }
